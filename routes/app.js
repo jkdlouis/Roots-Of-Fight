27e56ade0025b0d773bc102adadaf608c64fa9bf
Louis Huang <0563louishuang@gmail.com>
2017-02-07 00:57:09 -0800

    user model, schema, and user sign up backend set up

--- routes/app.js
+++ routes/app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 router.get('/', (req, res, next ) => {
    res.render('index');
