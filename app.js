c61c75f263571aef1b36f1e697ed1a7f3377b05f
Louis Huang <0563louishuang@gmail.com>
2017-03-05 17:41:22 -0800

    Adding 'Put' for request method

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, ,PUT, OPTIONS');
   next();
 });
 
